9644161863be59dd7f00733e1ffc92e0cafd2508
Marcel Grama <aace375@yahoo.ro>
2017-11-16 22:12:03 +0200

    Clear issues
    Clear issues
    Clear code
    Reponsive page

--- script/script.js
+++ script/script.js
@@ -1,26 +1,14 @@
-
-$("#search").keyup(function(){
-
-	var current_query = $("#search").val();
-
-	if(current_query!=""){
-
-
-	$(".thumbnails a").hide();
-	$(".thumbnails a").each(function(){
-
-		var current_keyword = $(this).attr("title");
-
-		if(current_keyword.indexOf(current_query)>=0){
-			$(this).show();
-		}
-
-
-	});
-
-}else{
-	$("#thumbnails a").show();
-}
-
-
+$("#search").keyup(function() {
+	var current_query = $("#search").val().toLowerCase();
+	if (current_query != "") {
+		$(".thumbnails a").hide();
+		$(".thumbnails a").each(function() {
+			var current_keyword = $(this).attr("title").toLowerCase();
+			if (current_keyword.indexOf(current_query) >= 0) {
+				$(this).show();
+			}
+		});
+	} else {
+		$("#thumbnails a").show();
+	}
 });
